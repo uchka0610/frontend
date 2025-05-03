0cd5682f86b2593be474ef5cb1bebd7ef429f096
MoonWarrior MGL <uuchkaa0610@gmail.com>
2025-05-03 17:53:02 +0800

    gg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,86 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      
-      keyframes: {
-
-        moveDown: {
-          '0%': { transform: 'translateY(0)' },
-          '100%': { transform: 'translateY(20px)' }, // you can adjust this
-        },
-      },
-      animation: {
-        moveDown: 'moveDown 5s ease-in-out infinite alternate',
-      },
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      scale: {
-        175: '1.75',
-        200: '2',
-        250: '2.5',
-        300: '3',
-      },
-      fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'], // 👈 Add this line
-      },
-    },
+  	extend: {
+  		keyframes: {
+  			moveDown: {
+  				'0%': {
+  					transform: 'translateY(0)'
+  				},
+  				'100%': {
+  					transform: 'translateY(20px)'
+  				}
+  			}
+  		},
+  		animation: {
+  			moveDown: 'moveDown 5s ease-in-out infinite alternate'
+  		},
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		scale: {
+  			'175': '1.75',
+  			'200': '2',
+  			'250': '2.5',
+  			'300': '3'
+  		},
+  		fontFamily: {
+  			montserrat: [
+  				'Montserrat',
+  				'sans-serif'
+  			]
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
